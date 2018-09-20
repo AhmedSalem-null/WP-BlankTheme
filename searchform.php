5102b1248695ca510fed51b9ab2a73a29e09330f
Ahmed Salem <ahmedsalem@theelephant.website>
2018-09-20 09:43:25 +0200

    Add files via upload

---
+++ searchform.php
@@ -0,0 +1,5 @@
+<form action="<?php bloginfo('siteurl'); ?>" id="searchform" method="get">
+        <label for="s" class="screen-reader-text">Search for:</label>
+        <input type="text" id="s" name="s" value="" placeholder="بحث الموقع" autocomplete="on" />
+        <button type="submit" id="searchsubmit" >Search</button>
+</form>
